c0ffcd4a821e65a592eabe13e1d66a8f08acde47
Caio Sampaio <casampai@c2r7p5.42sp.org.br>
2026-04-16 18:11:23 -0300

    strcat

--- ex02/ft_strcat.c
+++ ex02/ft_strcat.c
@@ -6,24 +6,35 @@
 /*   By: casampai <casampai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 16:23:39 by casampai          #+#    #+#             */
-/*   Updated: 2026/04/16 17:29:14 by casampai         ###   ########.fr       */
+/*   Updated: 2026/04/16 18:08:14 by casampai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
 
-char    *ft_strcat(char *dest, char *src)
+char	*ft_strcat(char *dest, char *src)
 {
 	char	*ptr_dest;
 	char	*ptr_src;
 	int		size;
 
-	ptr_dest = dest;
 	ptr_src = src;
-	size = sizeof(dest) / sizeof(dest[0])
+	size = sizeof(dest) - 1;
+	while(*ptr_dest)
+		ptr_dest++;
 	while (*ptr_src)
 	{
-		ptr_dest[size-1]
+		*ptr_dest = *ptr_src;
+		ptr_dest++;
+		ptr_src++;
 	}
+	return dest;
 }
 
+int	main(void)
+{
+	char string1[] = "42";
+	char string2[] = "NetWork";
+	char * str = ft_strcat(string1, string2);
+	return (0);
+}
