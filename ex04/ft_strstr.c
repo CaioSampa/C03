8df1d54876106adcf2418cea474aa80bdcb5fb5a
Caio Sampaio <casampai@c3r2p2.42sp.org.br>
2026-04-18 17:22:21 -0300

    tentativa de strstr

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,25 +6,29 @@
 /*   By: casampai <casampai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 16:10:48 by casampai          #+#    #+#             */
-/*   Updated: 2026/04/18 00:35:05 by casampai         ###   ########.fr       */
+/*   Updated: 2026/04/18 17:08:06 by casampai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	// size do tofind para comparar com as ocorrencias de cada caractere ser igual
-	// enquanto a str estiver sendo lida ele procura os caracteres do tofind verica com str e se forem igual acrescenta +1 para validar com o tamanho posteriormente
-	int		len_to_find;
 	char	*ptr_str;
-	int		count;
-	// int de index da primeira ocorrencia
-	len_to_find = 0;
-	ptr_str = str;
-	count = 0;
-	while (*(to_find + len_to_find))
-		len_to_find++;
-	while (*(ptr_str + count))
+	char	*ptr_to_find;
+
+	if (*to_find == '\0')
+		return (str);
+	while (*str)
 	{
-		// if(*ptr_str)
+		ptr_str = str;
+		ptr_to_find = to_find;
+		while (ptr_str && ptr_to_find && *ptr_str == *ptr_to_find)
+		{
+			ptr_str++;
+			ptr_to_find++;
+		}
+		if (*ptr_to_find == '\0')
+			return (str);
+		str++;
 	}
+	return (0);
 }
