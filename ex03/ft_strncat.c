75a8cfb275da49326a28af8dda1463714c34e2dd
Caio S <caiosampaio604@gmail.com>
2026-04-18 00:34:57 -0300

    fix ex02 ex03 add ex04

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -1,29 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: casampai <casampai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/17 14:54:45 by casampai          #+#    #+#             */
-/*   Updated: 2026/04/17 14:54:55 by casampai         ###   ########.fr       */
+/*   Created: 2026/04/17 14:54:37 by casampai          #+#    #+#             */
+/*   Updated: 2026/04/17 16:10:19 by casampai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcat(char *dest, char *src)
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	char	*ptr_dest;
-	char	*ptr_src;
+	char			*ptr_dest;
+	char			*ptr_src;
+	unsigned int	count;
 
 	ptr_src = src;
 	ptr_dest = dest;
+	count = 0;
 	while (*ptr_dest)
 		ptr_dest++;
-	while (*ptr_src)
+	while (*ptr_src && count < nb)
 	{
 		*ptr_dest = *ptr_src;
 		ptr_dest++;
 		ptr_src++;
+		count++;
 	}
+	*ptr_dest = '\0';
 	return (dest);
 }
---
+++ ex04/ft_strstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: casampai <casampai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/17 16:10:48 by casampai          #+#    #+#             */
+/*   Updated: 2026/04/17 23:01:37 by casampai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	// size do tofind para comparar com as ocorrencias de cada caractere ser igual
+	// enquanto a str estiver sendo lida ele procura os caracteres do tofind verica com str e se forem igual acrescenta +1 para validar com o tamanho posteriormente
+	int		len_to_find;
+	char	*ptr_str;
+	int		count;
+	// int de index da primeira ocorrencia
+	len_to_find = 0;
+	ptr_str = str;
+	count = 0;
+	while (*(to_find + len_to_find))
+		len_to_find++;
+	while (*(ptr_str + count))
+	{
+		if(*ptr_str)
+	}
+}
