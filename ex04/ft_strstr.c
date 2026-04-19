ba89e8f1c67ecf84879b59835c52f744cff883ea
Caio S <caiosampaio604@gmail.com>
2026-04-19 15:28:57 -0300

    fix ex04

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,27 +6,27 @@
 /*   By: casampai <casampai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 16:10:48 by casampai          #+#    #+#             */
-/*   Updated: 2026/04/18 17:08:06 by casampai         ###   ########.fr       */
+/*   Updated: 2026/04/19 15:24:04 by casampai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	char	*ptr_str;
-	char	*ptr_to_find;
+	char	*ps;
+	char	*pf;
 
 	if (*to_find == '\0')
 		return (str);
 	while (*str)
 	{
-		ptr_str = str;
-		ptr_to_find = to_find;
-		while (ptr_str && ptr_to_find && *ptr_str == *ptr_to_find)
+		ps = str;
+		pf = to_find;
+		while (*ps && *pf && *ps == *pf)
 		{
-			ptr_str++;
-			ptr_to_find++;
+			ps++;
+			pf++;
 		}
-		if (*ptr_to_find == '\0')
+		if (*pf == '\0')
 			return (str);
 		str++;
 	}
